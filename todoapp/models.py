04d730fb701f8d4dc0bd48809e834da54e5cdc71
Andrew Kisel <andrew@kisels.net>
2019-02-25 15:13:07 -0500

    Add task model, register it within admin

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=500)
+    priority = models.CharField(max_length=200)
