5a9ebc9d50a28fce4009465b7f5494219f9cccce
Andrew Kisel <andrew@kisels.net>
2019-02-25 14:50:53 -0500

    Add some test views and corresponding URL patterns

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('This worked!')
+
+
+def detail(request):
+    return HttpResponse('This is details page')
